d934d3f2c8d4b678d6396e5e6a72293737d38801
saikou9901 <saikou9901@gmail.com>
2020-05-23 14:24:16 +0900

    use graphql to add content into page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-export default () => (
+export default ({data}) => (
     <Layout>
         <React.Fragment>
-            <div>About page</div>
+            <h1>About</h1>
+            <h2>{data.site.siteMetadata.title}</h2>
+            <div>{data.site.siteMetadata.description}</div>
             <Link to="/">Back to home</Link>
         </React.Fragment>
     </Layout>
-)
+);
+
+export const query = graphql `
+query {
+  site {
+    siteMetadata {
+      author,
+      description,
+      title
+    }
+  }
+}
+`;
